0f0605b97b2323267ef1e676210ebdb7ec1bcec0
Sebastiao Trindade <sebatrindade@hotmail.com>
2024-12-06 11:18:38 -0300

    Scaffold Conta e Usuario

---
+++ db/migrate/20241206141646_create_conta.rb
@@ -0,0 +1,13 @@
+class CreateConta < ActiveRecord::Migration[7.0]
+  def change
+    create_table :conta do |t|
+      t.string :nome
+      t.text :descricao
+      t.decimal :valor
+      t.date :data_vencimento
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end

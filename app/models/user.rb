15a95a79b1500f3cdb7d4989e2df58479043d35f
Sebastiao Trindade <sebatrindade@hotmail.com>
2024-12-27 16:22:20 -0300

    Validação do signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true  
-  validates :password, presence: true, length: {minimum:6, maximum:6}
+  validates :name, presence: true, uniqueness: { case_sensitive: false, message: "já está cadastrado" }
+  validates :email, presence: true, uniqueness: { case_sensitive: false, message: "já está cadastrado" }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "não é válido" } 
+  validates :password, presence: true, confirmation: true, length: {minimum:6}
   
 end

44ec9180377e13e59afb11dd7fb4a159b48e0b33
Sebastiao Trindade <sebatrindade@hotmail.com>
2025-01-07 11:09:04 -0300

    Alterando contas com scaffold

--- app/models/contum.rb
+++ app/models/contum.rb
@@ -1,5 +1,2 @@
 class Contum < ApplicationRecord
-  validates :nome_empresa, :descricao, :valor, :data_vencimento, :status, presence: true
-  validates :valor, numericality: { greater_than: 0 }
-  validates :status, inclusion: { in: ['Pendente', 'Pago', 'Vencido'] }
 end
---
+++ app/views/conta/_contum.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! contum, :id, :nome_empresa, :descricao, :valor, :data_vencimento, :status, :created_at, :updated_at
+json.url contum_url(contum, format: :json)
